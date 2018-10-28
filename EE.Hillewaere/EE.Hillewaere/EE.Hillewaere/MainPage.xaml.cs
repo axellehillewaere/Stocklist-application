b6f0921a2004f50f5ee25a4fe217733dda195866
Axelle Hillewaere <axelle.hillewaere@hotmail.be>
2018-10-28 17:52:24 +0100

    Add solution to project

---
+++ EE.Hillewaere/EE.Hillewaere/EE.Hillewaere/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EE.Hillewaere
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
