6aa3ce8450d505917865a070f5390032c6fd55a4
Axelle Hillewaere <axelle.hillewaere@hotmail.be>
2018-12-23 13:47:36 +0100

    Add model classes for Product, Subcategory and Category

---
+++ EE.Hillewaere/EE.Hillewaere/EE.Hillewaere/Domain/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EE.Hillewaere.Domain.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public SubCategory SubCategory { get; set; }
+    }
+}
---
+++ EE.Hillewaere/EE.Hillewaere/EE.Hillewaere/Domain/Models/SubCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EE.Hillewaere.Domain.Models
+{
+    public class SubCategory
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public Category Category { get; set; }
+    }
+}
