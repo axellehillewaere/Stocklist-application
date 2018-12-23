6dc1fa04e2f2a0aa1422bca1e8b8661d1fc1dcd4
Axelle Hillewaere <axelle.hillewaere@hotmail.be>
2018-12-23 14:22:13 +0100

    Add CategoriesInMemoryService

---
+++ EE.Hillewaere/EE.Hillewaere/EE.Hillewaere/Domain/Services/CategoriesInMemoryService.cs
@@ -0,0 +1,126 @@
+﻿using EE.Hillewaere.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EE.Hillewaere.Domain.Services
+{
+    public class CategoriesInMemoryService
+    {
+        private static List<Category> categoryLists = new List<Category>
+        {
+            new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Drinks",
+                SubCategories = new List<SubCategory>
+                {
+                    new SubCategory
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "Soft drinks",
+                        Products = new List<Product>
+                        {
+                            new Product
+                            {
+                                Id = Guid.NewGuid(),
+                                Name = "Coca-Cola",
+                                Price = 10M
+                            },
+                            new Product
+                            {
+                                Id = Guid.NewGuid(),
+                                Name = "Coca-Cola Zero",
+                                Price = 12M
+                            }
+                        }
+                    },
+                    new SubCategory
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "Hot drinks",
+                        Products = new List<Product>
+                        {
+                            new Product
+                            {
+                                Id = Guid.NewGuid(),
+                                Name = "Coffee beans",
+                                Price = 15M
+                            },
+                            new Product
+                            {
+                                Id = Guid.NewGuid(),
+                                Name = "Cécémel",
+                                Price = 8M
+                            }
+                        }
+                    }
+                }
+            },
+            new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Snacks",
+                SubCategories = new List<SubCategory>
+                {
+                    new SubCategory
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "Chicken",
+                        Products = new List<Product>
+                        {
+                            new Product
+                            {
+                                Id = Guid.NewGuid(),
+                                Name = "Chicken Nuggets (Nuggiez)",
+                                Price = 14M
+                            }
+                        }
+                    },
+                    new SubCategory
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "Fish",
+                        Products = new List<Product>
+                        {
+                            new Product
+                            {
+                                Id = Guid.NewGuid(),
+                                Name = "Fish burger (Mora)",
+                                Price = 15M
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        public async Task<Category> GetCategoryList(Guid categoryId)
+        {
+            await Task.Delay(0);
+            return categoryLists.FirstOrDefault(c => c.Id == categoryId);
+        }
+
+        public async Task SaveCategoryList(Category category)
+        {
+            await Task.Delay(0);
+            var savedCategory = categoryLists.FirstOrDefault(c => c.Id == category.Id);
+            if (savedCategory == null)
+            {
+                savedCategory = category;
+                savedCategory.Id = Guid.NewGuid();
+                categoryLists.Add(savedCategory);
+            }
+            savedCategory.Name = category.Name;
+            savedCategory.SubCategories = category.SubCategories;
+        }
+
+        public async Task DeleteCategoryList(Guid categoryId)
+        {
+            await Task.Delay(0);
+            var category = categoryLists.FirstOrDefault(c => c.Id == categoryId);
+        }
+    }
+}
