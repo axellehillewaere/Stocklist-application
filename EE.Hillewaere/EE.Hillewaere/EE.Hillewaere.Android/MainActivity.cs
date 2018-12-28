e5eb89afd0b0eec27356235ddbd74e59a13c5e01
Axelle Hillewaere <axelle.hillewaere@hotmail.be>
2018-12-28 19:02:39 +0100

    Add splashscreen for UWP and Android

--- EE.Hillewaere/EE.Hillewaere/EE.Hillewaere.Android/MainActivity.cs
+++ EE.Hillewaere/EE.Hillewaere/EE.Hillewaere.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace EE.Hillewaere.Droid
 {
-    [Activity(Label = "EE.Hillewaere", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Stocklist", Theme = "@style/MainTheme")]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
